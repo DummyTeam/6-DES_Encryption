7698bb9e2ff9702c153780016ab307aa35c4b43a
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-03-18 22:05:30 +0400

    Added visual c++ solution with main.cpp

---
+++ Encryption/Main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+int main() {
+
+	std::string message = "This is an incredibly confidential message!";
+
+	std::cout << "Plain text (original message): " << message << std::endl;
+
+
+
+	std::cout << std::endl;
+	system("pause");
+	return 0;
+}
