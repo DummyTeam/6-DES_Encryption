145b1f96bbbbc2818cdbdac9346f73bb7a96efde
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-03-19 00:48:09 +0400

    Added slight change to main.cpp

--- Encryption/Main.cpp
+++ Encryption/Main.cpp
@@ -1,48 +1,53 @@
 ﻿#include<iostream>
 #include<string>
 
+#define ull unsigned long long 
 
-std::string strToBin(std::string message) {
+std::string decToBin(ull dec) {
 	int bin;
 
+	std::string charBin = "";
+
+	while (dec > 0)
+	{
+		bin = dec % 2;
+		charBin = std::to_string(bin) + charBin;
+		dec /= 2;
+	}
+
+	return charBin;
+}
+
+std::string strToBin(std::string message, bool isSpaced, bool isInitialPadding) {
 	std::string res = "";
 
 	for (size_t i = 0; i < message.size(); i++)
 	{
-		int decMessage = (int)message[i];
+		ull decMessage = (ull)message[i];
 
-		std::string charBin = "";
-
-		while (decMessage > 0)
-		{
-			bin = decMessage % 2;
-			charBin = std::to_string(bin) + charBin;
-			decMessage /= 2;
-		}
+		std::string charBin = decToBin(decMessage);
 
 		int initialPadding = 8 - charBin.size();
 
-		while (initialPadding--)
+		while (initialPadding-- && isInitialPadding)
 		{
 			charBin = "0" + charBin;
 		}
 
 		res += charBin;
-		res += (message.size() - 1 == i) ? "" : " ";
+		res += (message.size() - 1 == i || !isSpaced) ? "" : " ";
 	}
 
 	return res;
 }
 
 int main() {
-
-	std::string message = "This is an incredibly confidential message!"; //This is an incredibly confidential message!
-
+	std::string message = "133457799BBCDFF1"; //This is an incredibly confidential message!
 	std::cout << "Plain text (original message): " << message << std::endl;
+	//std::cout << strToBin(message, true, true) << std::endl;
+	
+	std::cout << decToBin(1383827165325090800) << std::endl;
 
-	std::cout << strToBin(message) << std::endl;
-
-	std::cout << std::endl;
 
 	system("pause");
 
