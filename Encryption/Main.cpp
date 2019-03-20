24e6673527f47406947670295b3a8855ece3ffd2
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-03-20 12:49:40 +0400

    Added subkey generation part of DES

--- Encryption/Main.cpp
+++ Encryption/Main.cpp
@@ -10,8 +10,6 @@ string BCH(string);
 string decToBin(ull);
 string strToBin(string, bool, bool);
 
-
-
 // a1 and a2 should be binary
 string XOR(string a1, string a2)
 {
@@ -66,6 +64,17 @@ string decToBin(ull dec) {
 	return charBin;
 }
 
+string toUpper(string str) {
+	for (string::iterator i = str.begin(); i != str.end(); i++)
+	{
+		if ((*i) >= 'a' && (*i) <= 'z')
+		{
+			(*i) -= ('a' - 'A');
+		}
+	}
+	return str;
+}
+
 string strToBin(string message, bool isSpaced, bool withInitialPadding) {
 	string res = "";
 
@@ -89,18 +98,94 @@ string strToBin(string message, bool isSpaced, bool withInitialPadding) {
 	return res;
 }
 
+string runThroughTable(string input, int arr[], int size) {
+	string res = "";
+
+	for (size_t i = 0; i < size; i++)
+	{
+		res += input[arr[i] - 1];
+	}
+
+	return res;
+}
+
+string shiftLeft(string input, int shiftValue) {
+	string res = input.substr(shiftValue);
+
+	for (size_t i = 0; i < shiftValue; i++)
+	{
+		char c = input[i];
+		res.push_back(c);
+	}
+
+	return res;
+}
+
 int main() {
-	//string message = "133457799BBCDFF1"; //This is an incredibly confidential message!
 
-	//cout << "Plain text (original message): " << message << endl;
+	// Initialized message and key values
+	string message = toUpper("0123456789ABCDEF");	 // In HEX. Pass through toUpper to make sure all in caps
+	string key = toUpper("133457799BBCDFF1");	 // In HEX. Pass through toUpper to make sure all in caps
 
-	//std::cout << strToBin(message, true, true) << std::endl;
+	// Converted HEX values to binary values
+	string binMessage = BCH(message);
+	string binKey = BCH(key);
 
-	//cout << (int)'A' << endl;
+	cout << binMessage << endl;
+	cout << binKey << endl;
 
-	cout << BCH("133457799BBCDFF1") << endl;
+	// TODO: if message is larger than 64bit divide them into 64bit parts
+
+	/// Key generation
+	// Initialized PC-1 and PC-2 table
+	int pc1[] = { 57, 49, 41, 33, 25, 17,  9,
+				   1, 58, 50, 42, 34, 26, 18,
+				  10,  2, 59, 51, 43, 35, 27,
+				  19, 11,  3, 60, 52, 44, 36,
+				  63, 55, 47, 39, 31, 23, 15,
+				   7, 62, 54, 46, 38, 30, 22,
+				  14,  6, 61, 53, 45, 37, 29,
+				  21, 13,  5, 28, 20, 12,  4 };
+
+	int pc2[] = { 14, 17, 11, 24, 1,   5,
+				  3, 28, 15,  6,  21, 10,
+				 23, 19, 12,  4,  26,  8,
+				 16,  7, 27, 20,  13,  2,
+				 41, 52, 31, 37,  47, 55,
+				 30, 40, 51, 45,  33, 48,
+				 44, 49, 39, 56,  34, 53,
+				 46, 42, 50, 36,  29, 32 };
+
+
+	// Run the binary key through PC-1 table to get permuted 56bit key
+	string permutedKey56 = runThroughTable(binKey, pc1, 56);
+
+	// Initialized arrays for C and D parts of the round keys
+	string C[17];
+	string D[17];
+
+	// Initialized array of final subkeys
+	string K[16];
+
+	// Splited the key into left and right halves, where each half has 28 bits.
+	C[0] = permutedKey56.substr(0, 28);
+	D[0] = permutedKey56.substr(28, 28);
+
+	// Initialized left-shift table
+	int leftShifts[] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
+
+	// Shifting the C and D parts to left to get other subkeys
+	for (size_t i = 1; i < 17; i++)
+	{
+		C[i] = shiftLeft(C[i - 1], leftShifts[i - 1]);
+		D[i] = shiftLeft(D[i - 1], leftShifts[i - 1]);
+
+		// Run the concatenated C and D parts through PC-2 table to get final subkeys
+		K[i - 1] = runThroughTable(C[i] + D[i], pc2, 48);
+	}
+
+	cout << endl;
 
 	system("pause");
-
 	return 0;
 }
